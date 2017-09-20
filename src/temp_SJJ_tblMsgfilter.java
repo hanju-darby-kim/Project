1b48a46c50f3bd80abc0d6185dcb2b685bbbde67
babmerger <alwayswp@naver.com>
2017-09-20 14:23:07 +0900

    재준 DTO
    jvaa

---
+++ src/temp_SJJ_tblMsgfilter.java
@@ -0,0 +1,28 @@
+
+public class temp_SJJ_tblMsgfilter {
+	private int seq;
+	private int filterEmployee;
+	private String filterword;
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public int getFilterEmployee() {
+		return filterEmployee;
+	}
+	public void setFilterEmployee(int filterEmployee) {
+		this.filterEmployee = filterEmployee;
+	}
+	public String getFilterword() {
+		return filterword;
+	}
+	public void setFilterword(String filterword) {
+		this.filterword = filterword;
+	}
+	
+}
+
+
