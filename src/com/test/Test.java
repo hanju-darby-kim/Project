515e20419a122c55e9c19febdd7e7817c16160e9
seulkijeon8903 <32083912+seulkijeon8903@users.noreply.github.com>
2017-09-20 12:10:23 +0900

    0
    0

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,14 +2,10 @@ package com.test;
 
 public class Test
 {
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		System.out.println("안녕하세요. 전슬기입니다");
-	}
-=======
+
     public static void main(String[] args)
     {
         System.out.println("테스트");
     }
->>>>>>> 71ae70a72e997d28e24d0427018a1e508819a6cc
+
 }
