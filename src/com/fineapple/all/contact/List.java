4039835b5fb99f954f975126fc9774262cf0c7ce
sunna920619 <sunna920619@users.noreply.github.com>
2017-09-21 20:12:02 +0900

    ''
    ''

--- src/com/fineapple/all/contact/List.java
+++ src/com/fineapple/all/contact/List.java
@@ -1,12 +1,15 @@
 package com.fineapple.all.contact;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fineapple.DTO.VEmployeeDTO;
 
 public class List extends HttpServlet {
 	
@@ -16,7 +19,7 @@ public class List extends HttpServlet {
 		
 		ContactService service = new ContactService();
 		
-		ArrayList<> service.getList();
+		ArrayList<VEmployeeDTO> vEmpList = service.getList();
 		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/company/pages/all/contact.jsp");
 		dispatcher.forward(req, resp);
