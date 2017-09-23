3af9e22c7c1a7a428ac193a2f569c5e6e64a8616
hanju-darby-kim <31398775+hanju-darby-kim@users.noreply.github.com>
2017-09-23 15:15:28 +0900

    ''
    ''

--- 프로젝트 문서/4-2. DML/직급Insert - 복사본 (4).sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT INTO tblposition (seq, POSITION) VALUES (1, '사장');
-insert into tblPosition (seq, position) values (2, '부장');
-insert into tblPosition (seq, position) values (3, '과장');
-insert into tblPosition (seq, position) values (4, '사원');
